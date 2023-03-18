a73c11f8e3fc9b84655395be567308518089db7e
ali <ali_hasan97@hotmail.com>
2023-03-18 03:20:31 -0500

    cleaned up some files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { demoComponent } from './demo.component';
-import { Demo2Component } from './demo2/demo2.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { GeneralCardComponent } from './general-card/general-card.component';
@@ -11,8 +9,6 @@ import { GeneralCardComponent } from './general-card/general-card.component';
 @NgModule({
   declarations: [
     AppComponent,
-    demoComponent,
-    Demo2Component,
     NavbarComponent,
     SidebarComponent,
     GeneralCardComponent
